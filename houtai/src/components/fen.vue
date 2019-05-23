27ad2ab2a948feab4d3c87e26502f9e7c190e2a9
SequoiaKing <tome.sf@foxmail.com>
2019-05-23 18:49:58 +0800

    删掉了一些注释

--- houtai/src/components/fen.vue
+++ houtai/src/components/fen.vue
@@ -6,17 +6,17 @@
 <script>
 
 export default {
-//import引入的组件需要注入到对象中才能使用
+
 components: {},
 data() {
-//这里存放数据
+
 return {
 
 };
 },
-//监听属性 类似于data概念
+
 computed: {},
-//监控data中的数据变化
+
 watch: {},
 //方法集合
 methods: {
