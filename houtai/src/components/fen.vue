1f70d07df5be769aa7ec2c43a2d309a34d38c532
Nimowang <1195869274@qq.com>
2019-05-27 10:20:08 +0800

    wang

--- houtai/src/components/fen.vue
+++ houtai/src/components/fen.vue
@@ -16,11 +16,10 @@ export default {
   watch: {},
   //方法集合
   methods: {},
-  //生命周期 - 创建完成（可以访问当前this实例）
   created() {},
-  //生命周期 - 挂载完成（可以访问DOM元素）
+
   mounted() {},
-  beforeCreate() {}, //生命周期 - 创建之前
+  beforeCreate() {}, 
   beforeMount() {}, //生命周期 - 挂载之前
   beforeUpdate() {}, //生命周期 - 更新之前
   updated() {}, //生命周期 - 更新之后
