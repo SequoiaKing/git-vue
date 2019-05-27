3163a387a9b9e4f5d13f1519d8565f036bdb627d
Nimowang <1195869274@qq.com>
2019-05-27 15:11:04 +0800

    删了备注

--- houtai/src/components/fen.vue
+++ houtai/src/components/fen.vue
@@ -20,9 +20,9 @@ export default {
 
   mounted() {},
   beforeCreate() {}, 
-  beforeMount() {}, //生命周期 - 挂载之前
-  beforeUpdate() {}, //生命周期 - 更新之前
-  updated() {}, //生命周期 - 更新之后
+  beforeMount() {}, 
+  beforeUpdate() {}, 
+  updated() {}, 
   beforeDestroy() {}, //生命周期 - 销毁之前
   destroyed() {}, //生命周期 - 销毁完成
   activated() {} //如果页面有keep-alive缓存功能，这个函数会触发
