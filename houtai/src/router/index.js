98e204628bf3a3c80853da0cf72915da486472f0
marsjoe <1121192537@qq.com>
2019-05-28 14:54:53 +0800

    lu

--- houtai/src/router/index.js
+++ houtai/src/router/index.js
@@ -5,11 +5,16 @@ import Getdata1 from '@/components/Getdata1'
 Vue.use(Router)
 
 export default new Router({
-  // routes: [
-  //   {
-  //     path: '/',
-  //     name: 'leftlist',
-  //     component: leftlist
-  //   }
-  // ]
+  routes: [
+    {
+      path: '/',
+      name: 'Getdata1',
+      component: Getdata1
+    },
+    {
+      path: '/order',
+      name: 'order',
+      component: order
+    }
+  ]
 })
